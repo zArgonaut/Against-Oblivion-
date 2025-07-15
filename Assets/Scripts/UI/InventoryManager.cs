8fd2f9b835d4de3309482ff7883e2756108cb09f
zArgonaut <caioa1012@gmail.com>
2025-07-15 02:07:44 -0300

    chore: cleaning project

--- Assets/Scripts/UI/InventoryManager.cs
+++ Assets/Scripts/UI/InventoryManager.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-
+using System.Collections.Generic;
 public class InventoryManager : MonoBehaviour
 {
     [Header("Background (não mexer)")]
